c9b52c88529be5dcf8bae04fb81ca97741e31ad6
Yukun <yukunpengba@gmail.com>
2019-01-06 17:28:14 -0600

    final changes

--- index.js
+++ index.js
@@ -10,7 +10,11 @@ app.get('/',function(req,res)
 
 io.on('connection',function(socket)
 {
-    console.log('a user connected');
+    socket.broadcast.emit('hi');
+    socket.on('chat message', function(msg){
+        console.log('message: ' + msg);
+        io.emit('chat message', msg);
+      });
     socket.on('disconnect',function()
     {
         console.log('user disconnected');
